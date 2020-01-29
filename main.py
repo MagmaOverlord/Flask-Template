2c9d73e02ff2739b6631f880418be135db3f6f82
MagmaOverlord <tomas.brejl@gmail.com>
2020-01-29 03:59:03 +0000

    Fixed up computer store

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import flask
 app = flask.Flask(__name__)
 
+items = {"PC":"Custom-Built PC with pre-chosen parts", "Macbook":"Apple's hit new PC - just like last years, but 0.1% faster!", "Yogabook":"For those who don't want a tablet"}
+
 @app.route('/')
 def home():
-    return flask.render_template('index.html', item="PC", definition="A custom-built PC (pre-chosen parts)")
+    return flask.render_template('index.html')
 
-#@app.route('/stuff/<item>')
+@app.route('/stuff/<item>')
+def lookup(item):
+    if item in items:
+        return flask.render_template('index.html', item = item, definition = items[item])
+    else:
+        return flask.render_template('index.html', item = 'not found', definition = "item doesn't exist")
 
 
 if __name__ == "__main__":
