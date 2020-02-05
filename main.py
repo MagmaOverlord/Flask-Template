b69021d403cac09fcb5f7f4134b988e966c56301
MagmaOverlord <tomas.brejl@outlook.com>
2020-02-04 20:05:18 -0800

    Fixed up store a bit

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import flask
 app = flask.Flask(__name__)
 
-items = {"PC":"Custom-Built PC with pre-chosen parts", "Macbook":"Apple's hit new PC - just like last years, but 0.1% faster!", "Yogabook":"For those who don't want a tablet"}
+items = {
+         "PC":"Custom-Built PC with pre-chosen parts", 
+         "Macbook":"Apple's hit new PC - just like last years, but 0.1% faster!", 
+         "Yogabook":"For those who don't want a tablet"
+         }
 
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', items = items)
 
 @app.route('/stuff/<item>')
 def lookup(item):
@@ -16,4 +20,4 @@ def lookup(item):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8081)
+    app.run()
